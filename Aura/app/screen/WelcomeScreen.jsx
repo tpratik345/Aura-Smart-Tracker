f6af12c294bcacb93dfaa57eb3b38b1584be6f30
tpratik345 <tpratik345@gmail.com>
2024-08-11 12:39:15 +0530

    Added nativewind + screens

---
+++ Aura/app/screen/WelcomeScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const WelcomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-3xl">WelcomeScreen</Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
+
+const styles = StyleSheet.create({})
